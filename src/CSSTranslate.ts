8c07246cd59084fd310a2a377c7beb257ac2dd52
Jaimish <infinity@s20.ai>
2022-03-22 19:09:26 +0530

    Adds fix for landscape center mode.

--- src/CSSTranslate.ts
+++ src/CSSTranslate.ts
@@ -1,5 +1,8 @@
 export default (position: number, metric: 'px' | '%', axis: 'horizontal' | 'vertical') => {
-    const positionPercent = position === 0 ? position : position + metric;
+    // const positionPercent = position === 0 ? position : position + metric;
+    
+    //to handle center mode on landscape for android and iOS
+    const positionPercent = position === 0 ? position: axis === 'horizontal' ? position + metric : position + 15 + metric;
     const positionCss = axis === 'horizontal' ? [positionPercent, 0, 0] : [0, positionPercent, 0];
     const transitionProp = 'translate3d';
 
